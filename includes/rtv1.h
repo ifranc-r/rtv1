b0f9143b0bc1e3eb2f0318e6f75a67349c081e54
Inti FRANC-REGIS <ifranc-r@e1r5p19.42.fr>
2017-03-09 15:39:06 +0100

    no lib

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -2,10 +2,8 @@
 # define RTV1_H
 # define FRACT_OL_H
 # include "../libft/libft.h"
-# include <SDL.h>
-//# include <SDL2/SDL.h>
+# include <SDL2/SDL.h>
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <errno.h>
@@ -30,8 +28,8 @@
 
 typedef struct		s_ray
 {
-	t_vect			o; // origin
-	t_vect			d; // direction
+	t_vect			o;
+	t_vect			d;
 	t_vect			inter;
 }					t_ray;
 
@@ -43,18 +41,18 @@ typedef struct		s_light
 
 typedef struct		s_plane
 {
-	t_vect			d; // axe
-	t_vect			n; //vector derector
-	t_vect			o; //origin
+	t_vect			d;
+	t_vect			n;
+	t_vect			o;
 	t_vect			inter;
 	t_color			color;
 }					t_plane;
 
 typedef struct		s_disc
 {
-	t_vect			d; // axe
-	t_vect			n; //vector derector
-	t_vect			o; //origin
+	t_vect			d;
+	t_vect			n;
+	t_vect			o;
 	double 			r;
 	t_vect			inter;
 	t_color			color;
@@ -62,25 +60,25 @@ typedef struct		s_disc
 
 typedef struct		s_sphere
 {
-	t_vect			c; // centre
-	double			r; // rayon de la sphere
+	t_vect			c;
+	double			r;
 	double 			t;
 	double 			t0;
 	double 			t1;
-	t_vect			n; // normal surface
-	t_vect			inter;// position of intersection
+	t_vect			n;
+	t_vect			inter;
 	t_color			color;
 }					t_sphere;
 
 typedef struct		s_cylinder
 {
-	t_vect			c; // centre
+	t_vect			c;
 	t_vect			axe;
 	t_vect			v;
 	double			m;
 	t_vect			n;
-	double			r; // rayon du disc
-	double			h; // hauteur
+	double			r;
+	double			h;
 	double 			t;
 	t_vect 			end;
 	t_disc			disc;
@@ -91,13 +89,13 @@ typedef struct		s_cylinder
 
 typedef struct		s_cone
 {
-	t_vect			c; // centre
+	t_vect			c;
 	t_vect			axe;
 	t_vect			v;
 	double			m;
 	t_vect			n;
-	double			r; // rayon du disc
-	double			h; // hauteur
+	double			r;
+	double			h;
 	double 			k;
 	double 			t;
 	t_vect			inter;
@@ -108,9 +106,9 @@ typedef struct		s_cone
 
 typedef struct		s_cam
 {
-	t_vect			campos; // camposition
-	t_vect			camdir; // cam direction vecto
-	t_vect			camup; // cam axis Y 
+	t_vect			campos;
+	t_vect			camdir;
+	t_vect			camup;
 	t_vect			camright; 
 }					t_cam;
 
@@ -155,8 +153,6 @@ typedef struct		s_all
 
 
 void 				sdl_and_funct(t_all *all);
-
-// parse
 int					num_c(char *str, char c);
 void				ft_exit(char *line);
 void				ft_notnum(char *str, char *line);
@@ -178,22 +174,20 @@ int 				ft_strcoord(char *str, char *src, int loop);
 void				draw(t_all *all, t_sdl *sdl);
 t_obj				parse_map(int fd);
 
-// intersection
 int					intersect_cylinder(t_ray *ray, t_cylinder *cylinder, double shadowlengh, int i);
 int 				intersect_cone(t_ray *ray, t_cone *cone, double shadowlengh, int i);
 int 				intersect_plane(t_ray *ray, t_plane *plane, double shadowlengh, int i);
 int					intersect_sphere(t_ray *ray, t_sphere *sphere, double shadowlengh, int i);
 int 				intersect_disc(t_ray *ray, t_disc *disc, double shadowlengh, int i);
-//sdl
+
 void				event(t_all *all, t_sdl *sdl, int file);
 void				sdl_close(t_sdl *sdl);
 void				sdl_err();
 void				sdl_init(t_sdl *sdl);
-//operation
+
 double 				triangle_area(t_vect p1, t_vect p2, t_vect p3);
 double				solveQuadratic(double a, double b, double c, int i);
 
-//obj and sceen
 void				init_sceen(t_all *all, int file);
 void				init_cam(t_cam *cam, int file);
 void				init_light(t_light *light, int file);
@@ -208,12 +202,12 @@ void				init_cone(t_cone *cone, int file);
 void				compute_ray(t_cam cam, t_ray *ray, int x, int y);
 void				init_cam(t_cam *cam, int file);
 
-int 				get_close_inter(t_ray *ray,t_obj *obj);
+int 				get_close_inter(t_ray *ray, t_obj *obj);
 t_vect				call_obj_n(t_obj obj, int num_obj);
 t_color				call_obj_color(t_obj obj, int num_obj);
 t_vect				call_obj_inter(t_obj obj, int num_obj);
 
-t_color				color_phong(t_color objct_color, t_light light, t_vect n,t_ray ray, t_vect inter);
+t_color				color_phong(t_color objct_color, t_light light, t_vect n, t_ray ray, t_vect inter);
 int 				shadow(t_vect inter, t_ray light, t_obj obj);
 
 #endif
